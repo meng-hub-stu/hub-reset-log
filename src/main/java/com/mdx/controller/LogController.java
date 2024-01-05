a608729a351e74c46724a64080c8fe82f36101f5
孟德磊 <mengdl@cheche365.com>
2024-01-05 15:30:45 +0800

    feat(hub-reset-log): 使用springboot自带的logback日志
    review: mengdx

--- src/main/java/com/mdx/controller/LogController.java
+++ src/main/java/com/mdx/controller/LogController.java
@@ -5,6 +5,8 @@ import com.mdx.service.ILogService;
 import com.mdx.util.R;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class LogController {
 
+    static Logger logger = LoggerFactory.getLogger(LogController.class);
+
     private final ILogService logService;
 
     @GetMapping(value = "{id}")
@@ -29,6 +33,13 @@ public class LogController {
         log.info("info测试数据入参->{}", id);
         log.warn("warn测试数据入参->{}", id);
         log.error("error测试数据入参->{}", id);
+        logger.info("使用new对象的方式实现->{}", id);
+        try {
+            throw new RuntimeException("打印异常");
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("错误日志", e);
+        }
         return R.ok(logService.getById(id));
     }
 
