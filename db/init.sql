c9e2d73e74f9e509e8210e2bd6fda3fdfdd4000c
孟德磊 <mengdl@cheche365.com>
2024-01-08 14:36:22 +0800

    feat(hub-reset-log): 配置log4j2
    review: mengdx

---
+++ db/init.sql
@@ -0,0 +1,50 @@
+CREATE TABLE `hub_dept` (
+  `id` bigint(64) NOT NULL COMMENT 'id',
+  `tenant_id` varchar(12) DEFAULT NULL COMMENT '租户id',
+  `dept_name` varchar(64) DEFAULT NULL COMMENT '标体',
+  `dept_id` bigint(64) DEFAULT NULL COMMENT '部门id',
+  `dept_alias` varchar(512) DEFAULT NULL COMMENT '部门别名',
+  `create_user` bigint(64) DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_user` bigint(64) DEFAULT NULL COMMENT '修改人',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `status` tinyint(1) DEFAULT '1' COMMENT '状态',
+  `is_deleted` tinyint(1) DEFAULT '0' COMMENT '是否已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='部门隔离表';
+
+CREATE TABLE `hub_log` (
+  `id` bigint(64) NOT NULL COMMENT 'id',
+  `tenant_id` varchar(12) DEFAULT NULL COMMENT '租户id',
+  `method` varchar(64) DEFAULT NULL COMMENT '方法',
+  `title` varchar(64) DEFAULT NULL COMMENT '标体',
+  `content` varchar(512) DEFAULT NULL COMMENT '内容',
+  `create_user` bigint(64) DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_user` bigint(64) DEFAULT NULL COMMENT '修改人',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `status` tinyint(1) DEFAULT '1' COMMENT '状态',
+  `is_deleted` tinyint(1) DEFAULT '0' COMMENT '是否已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='日志表';
+
+CREATE TABLE `hub_user` (
+  `id` bigint(64) NOT NULL COMMENT 'id',
+  `tenant_id` varchar(12) DEFAULT NULL COMMENT '租户id',
+  `birthday` datetime DEFAULT NULL COMMENT '生日',
+  `gender` varchar(1) DEFAULT NULL COMMENT '性别',
+  `user_name` varchar(32) NOT NULL COMMENT '用户名',
+  `password` varchar(256) NOT NULL COMMENT '密码',
+  `remark` varchar(32) DEFAULT NULL COMMENT '描述',
+  `mobile` varchar(11) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL COMMENT '姓名',
+  `create_user` bigint(64) DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_user` bigint(64) DEFAULT NULL COMMENT '修改人',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `status` tinyint(1) DEFAULT '1' COMMENT '状态',
+  `is_deleted` tinyint(1) DEFAULT '0' COMMENT '是否已删除',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_username` (`user_name`),
+  KEY `idx_name` (`name`) COMMENT '姓名'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
