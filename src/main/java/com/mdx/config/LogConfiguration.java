6f106bf0ffac99b176e7e29ccbb0bc92a9689f74
meng-hub-stu <17611103114@163.com>
2024-03-03 17:20:04 +0800

    feat(增加mysql额配置): 配置
    reiew: mengdx

--- src/main/java/com/mdx/config/LogConfiguration.java
+++ src/main/java/com/mdx/config/LogConfiguration.java
@@ -1,5 +1,7 @@
 package com.mdx.config;
 
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
@@ -38,5 +40,11 @@ public class LogConfiguration {
 		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
 		return executor;
 	}
-
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		//分页配置
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+		return interceptor;
+	}
 }
